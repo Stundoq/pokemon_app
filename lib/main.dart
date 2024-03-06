2ba9e0b1b2ecaa6d44618a1c8a91c82933f2fbce
frknblcc <frknblc1903@gmail.com>
2024-03-06 10:54:46 +0300

    deneme committttd

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      title: 'Poksadasemon',
+      title: 'Pokdsadasemon',
       debugShowCheckedModeBanner: false,
       home: const SplashPage(),
 
